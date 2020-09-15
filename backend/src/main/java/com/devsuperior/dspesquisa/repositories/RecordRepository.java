ae71bbb20987f512f2dcbf098915e39c56c1e56a
Clayton Pereira <clayton.pereira@outlook.com>
2020-09-15 20:34:41 -0300

    GET Records

--- backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
@@ -1,6 +1,11 @@
 package com.devsuperior.dspesquisa.repositories;
 
+import java.time.Instant;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dspesquisa.entities.Record;
@@ -8,5 +13,10 @@ import com.devsuperior.dspesquisa.entities.Record;
 //herda da Spring.data.JPA várias operaçãoes básicas para acessar o banco de dados (CRUD)
 @Repository //registra a classe RECORDREPOSITORY com um componente gerenciado pelo SpringBoot
 public interface RecordRepository extends JpaRepository<Record, Long>{
+
+	@Query("SELECT obj FROM Record obj WHERE "
+			+ "(coalesce(:minDate, null) IS NULL OR obj.moment >= :minDate) AND "
+			+ "(coalesce(:maxDate, null) IS NULL OR obj.moment <= :maxDate)")
+	Page<Record> findByMoments(Instant minDate, Instant maxDate, Pageable pageable);
 	
 }
--- backend/src/main/java/com/devsuperior/dspesquisa/resources/RecordResource.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/resources/RecordResource.java
@@ -1,10 +1,17 @@
 package com.devsuperior.dspesquisa.resources;
 
+import java.time.Instant;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dspesquisa.dto.RecordDTO;
@@ -39,5 +46,32 @@ public class RecordResource {
 		//método OK constroi um objeto se houver uma resposta. método Body inclui a resposta
 		return ResponseEntity.ok().body(newDTO);
 	}
-
+	
+	@GetMapping //annotation para avisar sobre a requisição
+	//funcão que recebe 6 parametros
+	public ResponseEntity<Page<RecordDTO>> findAll(
+			@RequestParam(value = "dateMin", defaultValue = "") String dateMin,
+			@RequestParam(value = "dateMax", defaultValue = "") String dateMax,
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "0") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "moment") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+		
+		//tratamento da data
+		Instant minDate = ("".equals(dateMin)) ? null : Instant.parse(dateMin);
+		Instant maxDate = ("".equals(dateMax)) ? null : Instant.parse(dateMax);
+		
+		//certifica que todos os registros serão retornados
+		if (linesPerPage == 0) {
+			linesPerPage = Integer.MAX_VALUE;
+		}
+		
+		//controla a paginação
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<RecordDTO> list = service.findByMoments(minDate, maxDate, pageRequest);
+		//método OK constroi um objeto se houver uma resposta. método Body inclui a resposta
+		return ResponseEntity.ok().body(list);
+	}
+ 
 }
